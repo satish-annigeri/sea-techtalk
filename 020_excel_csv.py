92f16652390d28dbbc8774a99bd997d1c872aa58
Satish Annigeri <satish.annigeri@gmail.com>
2026-02-03 03:21:28 -0800

    Minor improvements to 020_excel_csv.py

--- 020_excel_csv.py
+++ 020_excel_csv.py
@@ -1,6 +1,6 @@
 import marimo
 
-__generated_with = "0.19.0"
+__generated_with = "0.19.7"
 app = marimo.App(width="medium")
 
 
@@ -11,7 +11,21 @@ def _():
     import numpy as np
     import polars as pl
     import pandas as pd
-    return np, pd
+    return mo, np, pd
+
+
+@app.cell
+def _(mo):
+    mo.md(r"""
+    ## Reading results of support reactions exported from Staad.Pro
+
+    1. Read the data from Excel file
+    2. Modify the column names to remove the units from column headings
+    3. Drop one row from the heading that is not required
+    4. Set the data type of columns with numbers to `float`
+    5. Display the data
+    """)
+    return
 
 
 @app.cell
@@ -29,6 +43,14 @@ def _(pd):
     df[cols] = df[cols].astype("float64")
     df
     return cols, df
+
+
+@app.cell
+def _(mo):
+    mo.md(r"""
+    ## Print a summary of the data in columns with numerical values
+    """)
+    return
 
 
 @app.cell
@@ -50,6 +72,18 @@ def _(cols, df, pd):
 
 
 @app.cell
+def _(mo):
+    mo.md(r"""
+    ## Split the data into groups based on the value in column $F_y$
+
+    1. Determine the difference between the largest and smallest $F_y$ values
+    2. Define bins by dividing the range into 10 equal intervals
+    3. Create a new column with labels for rows based on the bin they fall within
+    """)
+    return
+
+
+@app.cell
 def _(df, np, pd):
     mult_of = 50.0
     vmin = np.floor(df['Fy'].min() / mult_of) * mult_of
@@ -58,12 +92,21 @@ def _(df, np, pd):
     bins = np.linspace(vmin, vmax, 6)
     labels = [f"F{_i}" for _i in range(1, 6)]
     df["class_intervals"] = pd.cut(df["Fy"], bins=bins, labels=labels, include_lowest=True)
-    df.sort_values("class_intervals")
+    df_sorted = df.sort_values(["class_intervals", "Fy"])
+    df_sorted
+    return (df_sorted,)
+
+
+@app.cell
+def _(mo):
+    mo.md(r"""
+    ## Display the data by striping alternate groups in a given colour
+    """)
     return
 
 
 @app.cell
-def _(df):
+def _(df_sorted):
     def stripe_groups(df, group_col):
         # Assign a group number for each block of identical values
         group_ids = df[group_col].ne(df[group_col].shift()).cumsum()
@@ -75,8 +118,7 @@ def _(df):
 
         return df.style.apply(row_style, axis=1)
 
-    df_striped = df.sort_values("class_intervals")
-    stripe_groups(df_striped, "class_intervals")
+    stripe_groups(df_sorted, "class_intervals")
     return
 
 
